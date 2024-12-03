ebb1b601d31fff40b9d99f684b15323bb642feb4
eduardoruas <eduardobritoruas@gmail.com>
2024-12-03 00:04:53 -0300

    segundo teste feito: saber se houve empate

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -1,9 +1,16 @@
-//#include <gtest/gtest.h>
+#include <gtest/gtest.h>
 #include "velha.hpp"
 
+//TESTE PARA SABER SE O X VENCE 
 TEST(JogoDaVelhaTest, TesteVitoriaX) {
     int tabuleiro[3][3] = {{1, 1, 1}, {0, 2, 0}, {2, 0, 2}};
     EXPECT_EQ(verifica_jogo(tabuleiro), 1);  // X vence
 }
 
+
+//TESTE PARA SABER SE HOUVE EMPATE 
+TEST(JogoDaVelhaTest, TesteEmpate) {
+    int tabuleiro[3][3] = {{1, 2, 1}, {1, 1, 2}, {2, 1, 2}};
+    EXPECT_EQ(verifica_jogo(tabuleiro), 0);  // Empate
+}
 
