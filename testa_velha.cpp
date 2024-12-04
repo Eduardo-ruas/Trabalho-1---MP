21f751b3102c53d488a02d351620c63932ffd35d
eduardoruas <eduardobritoruas@gmail.com>
2024-12-04 11:14:07 -0300

    Teste: Possibilidades de vitória do O

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -21,6 +21,8 @@ TEST_CASE("Jogo com apenas uma marcação retorna indefinido", "[verifica_jogo]"
     REQUIRE(verifica_jogo(tabuleiro) == -1);
 }
 
+//TESTES DAS POSSIBILIDADES DE VITORIA DE "X"
+
 TEST_CASE("Vitória do X na primeira linha", "[verifica_jogo]") {
     int tabuleiro[3][3] = {
         {1, 1, 1},
@@ -89,6 +91,80 @@ TEST_CASE("Vitória do X na diagonal secundária", "[verifica_jogo]") {
         {0, 2, 1},
         {0, 1, 0},
         {1, 2, 0}
+    };
+    REQUIRE(verifica_jogo(tabuleiro) == 1);
+}
+
+//TESTES DAS POSSIBILIDADES DE VITORIA DE "O"
+
+TEST_CASE("Vitória do O na primeira linha", "[verifica_jogo]") {
+    int tabuleiro[3][3] = {
+        {2, 2, 2},
+        {0, 1, 1},
+        {1, 0, 0}
+    };
+    REQUIRE(verifica_jogo(tabuleiro) == 1);
+}
+
+TEST_CASE("Vitória do O na segunda linha", "[verifica_jogo]") {
+    int tabuleiro[3][3] = {
+        {1, 0, 2},
+        {2, 2, 2},
+        {0, 1, 1}
+    };
+    REQUIRE(verifica_jogo(tabuleiro) == 1);
+}
+
+TEST_CASE("Vitória do O na terceira linha", "[verifica_jogo]") {
+    int tabuleiro[3][3] = {
+        {1, 0, 2},
+        {0, 1, 1},
+        {2, 2, 2}
+    };
+    REQUIRE(verifica_jogo(tabuleiro) == 1);
+}
+
+TEST_CASE("Vitória do O na primeira coluna", "[verifica_jogo]") {
+    int tabuleiro[3][3] = {
+        {2, 0, 1},
+        {2, 1, 0},
+        {2, 0, 1}
+    };
+    REQUIRE(verifica_jogo(tabuleiro) == 1);
+}
+
+TEST_CASE("Vitória do O na segunda coluna", "[verifica_jogo]") {
+    int tabuleiro[3][3] = {
+        {0, 2, 1},
+        {1, 2, 0},
+        {0, 2, 1}
+    };
+    REQUIRE(verifica_jogo(tabuleiro) == 1);
+}
+
+TEST_CASE("Vitória do O na terceira coluna", "[verifica_jogo]") {
+    int tabuleiro[3][3] = {
+        {0, 1, 2},
+        {1, 0, 2},
+        {0, 1, 2}
+    };
+    REQUIRE(verifica_jogo(tabuleiro) == 1);
+}
+
+TEST_CASE("Vitória do O na diagonal principal", "[verifica_jogo]") {
+    int tabuleiro[3][3] = {
+        {2, 0, 1},
+        {0, 2, 0},
+        {1, 0, 2}
+    };
+    REQUIRE(verifica_jogo(tabuleiro) == 1);
+}
+
+TEST_CASE("Vitória do O na diagonal secundária", "[verifica_jogo]") {
+    int tabuleiro[3][3] = {
+        {0, 1, 2},
+        {0, 2, 0},
+        {2, 1, 0}
     };
     REQUIRE(verifica_jogo(tabuleiro) == 1);
 }
