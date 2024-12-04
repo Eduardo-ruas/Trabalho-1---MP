516589ab9b2a609a67160e7890709831a939ee16
eduardoruas <eduardobritoruas@gmail.com>
2024-12-04 10:57:43 -0300

    Teste: Jogo com apenas uma marcação

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -2,3 +2,21 @@
 #include "velha.hpp"
 #define CATCH_CONFIG_MAIN
 
+//TESTE DE JOGO VAZIO - VERIFICA SE AS CASAS ESTAO VAZIAS 
+TEST_CASE("Jogo vazio retorna indefinido", "[verifica_jogo]") {
+    int tabuleiro[3][3] = {
+        {0, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0}
+    };
+    REQUIRE(verifica_jogo(tabuleiro) == -1);
+}
+
+TEST_CASE("Jogo com apenas uma marcação retorna indefinido", "[verifica_jogo]") {
+    int tabuleiro[3][3] = {
+        {1, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0}
+    };
+    REQUIRE(verifica_jogo(tabuleiro) == -1);
+}
