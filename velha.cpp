a89647aa6b9e6365f91038271b0064c1e97a16ea
eduardoruas <eduardobritoruas@gmail.com>
2024-12-06 12:58:41 -0300

    Teste feitos

--- velha.cpp
+++ velha.cpp
@@ -1,25 +1,7 @@
 #include "velha.hpp"
 
 int verifica_jogo(int tabuleiro[3][3]) {
-    // 1. Verificar se há um vencedor
-    for (int i = 0; i < 3; ++i) {
-        if (tabuleiro[i][0] == tabuleiro[i][1] && tabuleiro[i][1] == tabuleiro[i][2] && tabuleiro[i][0] != 0) {
-            return tabuleiro[i][0];  // Retorna o vencedor (1 ou 2)
-        }
-        if (tabuleiro[0][i] == tabuleiro[1][i] && tabuleiro[1][i] == tabuleiro[2][i] && tabuleiro[0][i] != 0) {
-            return tabuleiro[0][i];  // Retorna o vencedor (1 ou 2)
-        }
-    }
-
-    // Verificar as diagonais
-    if (tabuleiro[0][0] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][2] && tabuleiro[0][0] != 0) {
-        return tabuleiro[0][0];
-    }
-    if (tabuleiro[0][2] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][0] && tabuleiro[0][2] != 0) {
-        return tabuleiro[0][2];
-    }
-
-    // 2. Verificar se todas as casas do tabuleiro são iguais
+    // 1. Verificar se todas as casas do tabuleiro são iguais
     bool todas_iguais = true;
     int primeiro = tabuleiro[0][0];  // Valor da primeira casa
     if (primeiro == 0) {
@@ -40,6 +22,24 @@ int verifica_jogo(int tabuleiro[3][3]) {
 
     if (todas_iguais) {
         return -2;  // Jogo impossível
+    }
+
+    // 2. Verificar se há um vencedor
+    for (int i = 0; i < 3; ++i) {
+        if (tabuleiro[i][0] == tabuleiro[i][1] && tabuleiro[i][1] == tabuleiro[i][2] && tabuleiro[i][0] != 0) {
+            return tabuleiro[i][0];  // Retorna o vencedor (1 ou 2)
+        }
+        if (tabuleiro[0][i] == tabuleiro[1][i] && tabuleiro[1][i] == tabuleiro[2][i] && tabuleiro[0][i] != 0) {
+            return tabuleiro[0][i];  // Retorna o vencedor (1 ou 2)
+        }
+    }
+
+    // Verificar as diagonais
+    if (tabuleiro[0][0] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][2] && tabuleiro[0][0] != 0) {
+        return tabuleiro[0][0];
+    }
+    if (tabuleiro[0][2] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][0] && tabuleiro[0][2] != 0) {
+        return tabuleiro[0][2];
     }
 
     // 3. Verificar se há posições vazias (jogo indefinido)
